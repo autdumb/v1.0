ca864299bd1369833b4e25e025904d869e431379
Autumn Liu <aut.tea7@gmail.com>
2024-08-14 15:22:57 -0500

    Add files via upload

---
+++ js/formrequest.js
@@ -0,0 +1,12 @@
+function submit() {
+	var inputq1 = document.getElementById("question1").value
+	var inputq2 = document.getElementById("question2").value
+	var inputq3 = document.getElementById("question3").value
+	var inputq4 = document.getElementById("question4").value
+	var inputq5 = document.getElementById("question5").value
+	// console.log(inputq1)
+	// console.log(inputq2)
+	// console.log(inputq3)
+	// console.log(inputq4)
+	// console.log(inputq5)
+}
