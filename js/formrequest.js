4bb133c819402f4e18012f68a79ceadd816a2f1d
Autumn Liu <aut.tea7@gmail.com>
2024-08-14 23:25:30 -0500

    text edits

--- js/formrequest.js
+++ js/formrequest.js
@@ -9,4 +9,19 @@ function submit() {
 	// console.log(inputq3)
 	// console.log(inputq4)
 	// console.log(inputq5)
+
+	// fetch('https://script.google.com/macros/s/AKfycbwnb-mSA_vX7rBcay3KO28ESMpDhmzNBG1FkTBGTNEhzvGm3xSr2afdY8lievKq0VF0Ig/exec', {  // Replace with your Google Apps Script URL
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/x-www-form-urlencoded',
+    //             },
+    //             body: 'input=' + encodeURIComponent(userInput),
+    //         })
+    //         .then(response => response.text())
+    //         .then(data => {
+    //             console.log(data); // Handle the response data
+    //         })
+    //         .catch(error => {
+    //             console.error('Error:', error);
+    //         });
 }
